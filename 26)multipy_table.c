fa41366e362be0f4bc8f45302207def214c581b6
Taniya Gusain <138493649+Cyberphile@users.noreply.github.com>
2023-09-07 02:43:13 +0530

    Update 26)multipy_table.c

--- 26)multipy_table.c
+++ 26)multipy_table.c
@@ -11,32 +11,3 @@ int main(){
 
 }
 
-#include<stdio.h>
-int main(){
-    int num,i,n=0;
-    printf("Enter 10 numbers: ");
-    for(i =1; i<=5; i++){
-        scanf("%d", &num);
-        if(num % 2 == 0){
-            n++;
-        }
-    }
-    printf("Number of even numbers: %d\n", n);
-    
-    return 0;
-}
-
-#include<stdio.h>
-int main(){
-    int num,i,n=1;
-    printf("Enter a number: ");
-    scanf("%d", &num);
-    for(i = num; i>=1; i--){
-        n *= i;
-    }
-    printf("Factorial of %d is %d", num,n);
-    
-    return 0;
-}
-
-
